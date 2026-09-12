90c5e4e022116dda0aad581bb0d9fca9cc97a949
Riya Mehta <167575288+1754riya@users.noreply.github.com>
2026-09-12 16:52:13 +0530

    Time: 1 ms (6.92%), Space: 7.6 MB (98.89%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0){
+            return false;
+        }
+        long long power=1;
+        while(power<=n){
+            if(power==n){
+                return true;
+            }
+            power=power*2;
+        }
+       return false;
+    }
+};
