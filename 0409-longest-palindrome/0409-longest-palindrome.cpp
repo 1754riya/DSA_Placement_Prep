8536567deadf073d1bb50d7df805cd60eeb0ea62
Riya Mehta <167575288+1754riya@users.noreply.github.com>
2026-09-14 01:11:53 +0530

    Time: 0 ms (100%), Space: 9.3 MB (41.02%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        if(s.size()==1){
+            return 1;
+        }
+        unordered_map<char,int>freq;
+        for(char ch:s){
+            freq[ch]++;
+        }
+        int ans=0;
+        bool odd=false;
+        for(auto &it:freq){
+            if(it.second%2==0){
+                ans+=it.second;
+            }else{
+                ans+=it.second-1;
+                odd=true;
+            }
+        }
+        if(odd){
+            ans++;
+        }
+        return ans;
+    }
+};
